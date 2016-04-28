86351259b5584475b99a0054e67c3aba3b135208
Kelly Farley <farleykc@gmail.com>
2016-04-28 12:04:36 -0500

    some images added

--- controller.rb
+++ controller.rb
@@ -8,15 +8,15 @@ end
 get '/direction/:choice' do
 	case params[:choice]
 	when "west"
-		@websitesettings = ["", "You find another group of kids from the party. They say they know a place they can go for the night until they can call for help. They seem a little suspicious though… do you trust them?", "Join_the_group", "Keep_walking"]
+		@websitesettings = ["http://static1.squarespace.com/static/50c8e51ce4b052a90587d0c7/t/5245a7b9e4b0b15467cfe175/1380296634418/diy-kids-costumes-2010.jpg", "You find another group of kids from the party. They say they know a place they can go for the night until they can call for help. They seem a little suspicious though… do you trust them?", "Join_the_group", "Keep_walking"]
 		erb :index	
 
 	when "east"
-		@websitesettings = ["", "You walk towards the light, like some sort of dazed firefly.  The light leads to a banged up minivan that looks very similar to the way your parents’ car looked after you tried parallel parking for the first time.  You see the faces of a kindly elderly couple.  The driver smiles at you with her toothless gums.", "get_into_car", "Walk_away"]
+		@websitesettings = ["https://s-media-cache-ak0.pinimg.com/736x/c4/c0/db/c4c0db64d46301d6f83ad26b87044739.jpg", "You walk towards the light, like some sort of dazed firefly.  The light leads to a banged up minivan that looks very similar to the way your parents’ car looked after you tried parallel parking for the first time.  You see the faces of a kindly elderly couple.  The driver smiles at you with her toothless gums.", "get_into_car", "Walk_away"]
 		erb :index
 
 	when "Join_the_group"
-		@websitesettings = ["", "You join the group, and together you find a cabin in the woods. “Sweet place! Let’s stay here for a while,” Someone says. It looks cozy and welcoming. “What could go wrong?” you agree.  Upon entering the house, you are greeted by a small old woman, who promptly ushers you into the kitchen… specifically the oven. It dawns upon you what this trap is; you should’ve known better (the walls were made of gingerbread… yeah). Go back and try again."]
+		@websitesettings = ["https://cubelight.files.wordpress.com/2013/05/hansel-and-gretel-witch-hunters-candy-house.jpg", "You join the group, and together you find a cabin in the woods. “Sweet place! Let’s stay here for a while,” Someone says. It looks cozy and welcoming. “What could go wrong?” you agree.  Upon entering the house, you are greeted by a small old woman, who promptly ushers you into the kitchen… specifically the oven. It dawns upon you what this trap is; you should’ve known better (the walls were made of gingerbread… yeah). Go back and try again."]
 		erb :ending
 
 	when "Keep_walking"
